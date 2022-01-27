7e5e2568b258e68d1c1cecf78444488d4dbef4f0
Nam Nguyen <namnguyenthanh.developer@gmail.com>
2022-01-27 17:20:39 +0700

    add Promo section to HomePage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Hero } from '../components';
 import { Featured } from '../components/Featured';
+import Promo from '../components/Promo';
 
 const HomePage = () => {
   return (
     <>
       <Hero />
       <Featured/>
+      <Promo/>
     </>
   );
 };
